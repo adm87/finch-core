44266efc3f48ee0ae2ee1d963b80e05c7a9d9c89
Aaron Malley <aaron.d.malley@gmail.com>
2025-07-31 09:15:07 -0300

    systems can now be prioritized on registration with a world

--- ecs/world.go
+++ ecs/world.go
@@ -1,49 +1,61 @@
 package ecs
 
 import (
+	"slices"
+
 	"github.com/adm87/finch-core/errors"
 	"github.com/adm87/finch-core/time"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+type WorldSys[T System] struct {
+	Sys  T
+	Prio int
+}
+
 type World struct {
-	entities map[EntityID]*Entity
-
-	updateSystems []UpdateSystem
-	renderSystems []RenderSystem
-
+	entities      map[EntityID]*Entity
 	systemsByType map[SystemType]System
+	updateSystems []WorldSys[UpdateSystem]
+	renderSystems []WorldSys[RenderSystem]
 }
 
 func NewWorld() *World {
 	return &World{
 		entities:      make(map[EntityID]*Entity),
-		updateSystems: []UpdateSystem{},
-		renderSystems: []RenderSystem{},
 		systemsByType: make(map[SystemType]System),
+		updateSystems: []WorldSys[UpdateSystem]{},
+		renderSystems: []WorldSys[RenderSystem]{},
 	}
 }
 
-func (w *World) RegisterSystems(systems ...System) *World {
-	for _, system := range systems {
-		if system == nil {
-			panic(errors.NewNilError("cannot register nil system"))
-		}
-		if _, exists := w.systemsByType[system.Type()]; exists {
-			panic(errors.NewDuplicateError("system already registered"))
-		}
-
-		w.systemsByType[system.Type()] = system
-
-		switch sys := system.(type) {
-		case UpdateSystem:
-			w.updateSystems = append(w.updateSystems, sys)
-		case RenderSystem:
-			w.renderSystems = append(w.renderSystems, sys)
-		default:
-			panic(errors.NewInvalidArgumentError("system does not implement UpdateSystem or RenderSystem"))
-		}
+// RegisterSystem registers a system in the world.
+//
+// Panics if the system is nil, if the system type is already registered, or if the system does not implement UpdateSystem or RenderSystem.
+//
+// Systems will be stored based on their type (UpdateSystem or RenderSystem) and will be executed in the order of their registered priority.
+//
+// TODO: Currently if a system is both an UpdateSystem and RenderSystem, it uses the same priority for both categorizations. This might be addressed later.
+func (w *World) RegisterSystem(system System, priority int) *World {
+	if system == nil {
+		panic(errors.NewNilError("cannot register nil system"))
 	}
+	if _, exists := w.systemsByType[system.Type()]; exists {
+		panic(errors.NewDuplicateError("system already registered"))
+	}
+	if sys := system.(UpdateSystem); sys != nil {
+		w.updateSystems = append(w.updateSystems, WorldSys[UpdateSystem]{Sys: sys, Prio: priority})
+		slices.SortFunc(w.updateSystems, func(a, b WorldSys[UpdateSystem]) int {
+			return a.Prio - b.Prio
+		})
+	}
+	if sys := system.(RenderSystem); sys != nil {
+		w.renderSystems = append(w.renderSystems, WorldSys[RenderSystem]{Sys: sys, Prio: priority})
+		slices.SortFunc(w.renderSystems, func(a, b WorldSys[RenderSystem]) int {
+			return a.Prio - b.Prio
+		})
+	}
+	w.systemsByType[system.Type()] = system
 	return w
 }
 
@@ -91,8 +103,8 @@ func (w *World) GetSystem(systemType SystemType) (System, bool) {
 
 func (w *World) Update(t time.Time) error {
 	for _, system := range w.updateSystems {
-		entities := w.getEntitiesForSystem(system)
-		if err := system.Update(entities, t); err != nil {
+		entities := w.getEntitiesForSystem(system.Sys)
+		if err := system.Sys.Update(entities, t); err != nil {
 			return err
 		}
 	}
@@ -101,8 +113,8 @@ func (w *World) Update(t time.Time) error {
 
 func (w *World) Render(buffer *ebiten.Image, view ebiten.GeoM) error {
 	for _, system := range w.renderSystems {
-		entities := w.getEntitiesForSystem(system)
-		if err := system.Render(entities, buffer, view); err != nil {
+		entities := w.getEntitiesForSystem(system.Sys)
+		if err := system.Sys.Render(entities, buffer, view); err != nil {
 			return err
 		}
 	}
@@ -124,7 +136,7 @@ func (w *World) getEntitiesForSystem(system System) []Entity {
 
 func (w *World) Clear() {
 	w.entities = make(map[EntityID]*Entity)
-	w.updateSystems = []UpdateSystem{}
-	w.renderSystems = []RenderSystem{}
+	w.updateSystems = []WorldSys[UpdateSystem]{}
+	w.renderSystems = []WorldSys[RenderSystem]{}
 	w.systemsByType = make(map[SystemType]System)
 }
