e3791dd1fd9db1a056514125fac3ead06babbc9a
Aaron Malley <aaron.d.malley@gmail.com>
2025-08-14 15:32:06 -0300

    added select linq methods

---
+++ linq/select.go
@@ -0,0 +1,31 @@
+package linq
+
+func Select[T comparable](source []T, selector func(T) bool) []T {
+	var result []T
+	for _, item := range source {
+		if selector(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func SelectKeys[K comparable, V any](source map[K]V, selector func(K, V) bool) []K {
+	var result []K
+	for key, value := range source {
+		if selector(key, value) {
+			result = append(result, key)
+		}
+	}
+	return result
+}
+
+func SelectValues[K comparable, V any](source map[K]V, selector func(K, V) bool) []V {
+	var result []V
+	for key, value := range source {
+		if selector(key, value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
