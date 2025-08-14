6c8dac85afc51bc571551eae2a1c6b2432e06a6e
Aaron Malley <aaron.d.malley@gmail.com>
2025-08-14 09:34:09 -0300

    constraint clean up

--- math/clamp.go
+++ math/clamp.go
@@ -1,7 +1,7 @@
 package math
 
 // Clamp restricts a value to be within a specified range.
-func Clamp[T number](value, min, max T) T {
+func Clamp[T isNumber](value, min, max T) T {
 	if value < min {
 		return min
 	}
@@ -12,7 +12,7 @@ func Clamp[T number](value, min, max T) T {
 }
 
 // Sign returns -1 for negative values, 1 for positive values, and 0 for zero.
-func Sign[T number](value T) int {
+func Sign[T isInt | isFloat](value T) int {
 	if value < 0 {
 		return -1
 	} else if value > 0 {
--- math/consts.go
+++ math/consts.go
@@ -5,6 +5,18 @@ const (
 	RadToDeg = 57.29577951308232    // 180 / Pi
 )
 
-type number interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~float32 | ~float64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+type isInt interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type isUint interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type isFloat interface {
+	~float32 | ~float64
+}
+
+type isNumber interface {
+	isInt | isUint | isFloat
 }
